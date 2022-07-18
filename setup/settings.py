ab7720fbfd2d5c74385f63b69906fa310da36fa7
fksolutionssv <fksolutionssv@gmail.com>
2022-07-18 10:29:51 -0300

    Configuração da pasta templates no settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -1,5 +1,5 @@
-import os
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,7 +43,7 @@ ROOT_URLCONF = 'setup.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
