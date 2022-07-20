9beae5d97255bff8710d01e76d2eeeb076c4f8b8
FabioLuizSV <fabio.tecmar@gmail.com>
2022-07-20 07:41:33 -0300

    Criado a Model Cardapio

--- cardapio/models.py
+++ cardapio/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Cardapio(models.Model):
+    """Definição de modelo para Cardapio."""
+
+    nomeCardapio = models.CharField(max_length=60, name='Cardápio')
+    bloqueado = models.BooleanField()
+
+    class Meta:
+        """Definição de Meta para Cardapio."""
+
+        verbose_name = 'Cardapio'
+        verbose_name_plural = 'Cardapios'
+
+    def __str__(self):
+        return self.nomeCardapio
